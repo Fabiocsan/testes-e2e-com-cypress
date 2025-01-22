b9612987b4a1cae5d4836300907f7833fa0db01f
Fabio Santos <fabiocsan@gmail.com>
2025-01-22 09:19:40 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://notes-serverless-app.com',
+    baseUrl: 'https://front.serverest.dev',
     requestTimeout: 15000,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
